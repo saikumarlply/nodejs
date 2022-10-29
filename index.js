22ae4e5297f628e5065f24155c110de0e608bd0c
pakashivaji <paka.shivaji@arable.com>
2022-10-29 23:35:18 +0530

    Pushing to heroku 2

--- index.js
+++ index.js
@@ -18,6 +18,26 @@ mongoose.connect(process.env.DB_CONNECT,
 () => console.log('connected to mongo db'))
 // Route middleware
 app.use(express.json());
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    
+    next();
+});
 app.use('/api/user', authRoute);
 app.use('/api/posts',postRoute);
 app.use('/api/user',userRoute);
